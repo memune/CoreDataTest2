5c222c80f7f7c3469474a2fbe4d9173eef90406c
memune <memuune@gmail.com>
2022-02-27 22:59:58 +0900

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by Taehoon Park on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
